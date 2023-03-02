2f860c04a71a4cdcefcef1a6c97cdc556a027e66
George Gale <gmgale@icloud.com>
2023-03-02 15:44:03 +0100

    Added handlers.

--- main.go
+++ main.go
@@ -3,7 +3,24 @@ package main
 import (
   "github.com/gorilla/mux"
   "net/http"
+  "encoding/json"
 )
+
+type Product struct {
+    Id int
+    Name string
+    Slug string
+    Description string
+}
+
+var products = []Product{
+  Product{Id: 1, Name: "World of Authcraft", Slug: "world-of-authcraft", Description : "Battle bugs and protect yourself from invaders while you explore a scary world with no security"},
+  Product{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description : "Explore the depths of the sea in this one of a kind underwater experience"},
+  Product{Id: 3, Name: "Dinosaur Park", Slug : "dinosaur-park", Description : "Go back 65 million years in the past and ride a T-Rex"},
+  Product{Id: 4, Name: "Cars VR", Slug : "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
+  Product{Id: 5, Name: "Robin Hood", Slug: "robin-hood", Description : "Pick up the bow and arrow and master the art of archery"},
+  Product{Id: 6, Name: "Real World VR", Slug: "real-world-vr", Description : "Explore the seven wonders of the world in VR"}}
+
 
 func main() {
   r := mux.NewRouter()
@@ -21,3 +38,43 @@ func main() {
 var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
   w.Write([]byte("Not Implemented"))
 })
+
+/* The status handler will be invoked when the user calls the /status route
+   It will simply return a string with the message "API is up and running" */
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+  w.Write([]byte("API is up and running"))
+})
+
+/* The products handler will be called when the user makes a GET request to the /products endpoint.
+   This handler will return a list of products available for users to review */
+var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+  // Here we are converting the slice of products to JSON
+  payload, _ := json.Marshal(products)
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write([]byte(payload))
+})
+
+/* The feedback handler will add either positive or negative feedback to the product
+   We will impliment DB stuff later
+   so that as long as the request is successful and we can match a product to our catalog of products we'll return an OK status. */
+var AddFeedbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+  var product Product
+  vars := mux.Vars(r)
+  slug := vars["slug"]
+
+  for _, p := range products {
+      if p.Slug == slug {
+          product = p
+      }
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  if product.Slug != "" {
+    payload, _ := json.Marshal(product)
+    w.Write([]byte(payload))
+  } else {
+    http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+  }
+})
+
