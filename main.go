b5cb2ddca1f4955704821d7b8ad4bddf5332478d
George Gale <gmgale@icloud.com>
2023-03-02 15:33:55 +0100

    Added basic routing.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "encoding/json"
   "github.com/gorilla/mux"
   "net/http"
 )
@@ -10,7 +9,15 @@ func main() {
   r := mux.NewRouter()
 
   r.Handle("/", http.FileServer(http.Dir("./views/")))
+  r.Handle("/status", NotImplemented).Methods("GET")
+  r.Handle("/products", NotImplemented).Methods("GET")
+  r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+
   r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
   http.ListenAndServe(":8080", r)
 }
+
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+  w.Write([]byte("Not Implemented"))
+})
